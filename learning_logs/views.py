01af6eb77692e4a609a27c7da89afd4b7d88c165
Gideon Adeti <gideonadeti0@gmail.com>
2025-09-28 21:00:16 +0000

    feat: add login requirement to learning_logs views and update settings
    - Apply @login_required decorator to topics, topic, new_topic, new_entry, and edit_entry views to enforce user authentication.
    - Update settings.py to define LOGIN_URL for redirecting unauthenticated users to the login page.

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
 from .models import Topic, Entry
 from .form import TopicForm, EntryForm
@@ -10,6 +11,7 @@ def index(request):
     return render(request, "learning_logs/index.html")
 
 
+@login_required
 def topics(request):
     """Show all topics."""
 
@@ -19,6 +21,7 @@ def topics(request):
     return render(request, "learning_logs/topics.html", context)
 
 
+@login_required
 def topic(request, topic_id):
     """Show a single topic and all its entries."""
 
@@ -29,6 +32,7 @@ def topic(request, topic_id):
     return render(request, "learning_logs/topic.html", context)
 
 
+@login_required
 def new_topic(request):
     """Add a new topic."""
 
@@ -50,6 +54,7 @@ def new_topic(request):
     return render(request, "learning_logs/new_topic.html", context)
 
 
+@login_required
 def new_entry(request, topic_id):
     """Add a new entry for a particular topic."""
 
@@ -75,6 +80,7 @@ def new_entry(request, topic_id):
     return render(request, "learning_logs/new_entry.html", context)
 
 
+@login_required
 def edit_entry(request, entry_id):
     """Edit an existing entry."""
 
