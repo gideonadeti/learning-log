4294a3aeb89f62e31926deda145cd0c8911450a4
Gideon Adeti <gideonadeti0@gmail.com>
2025-09-28 23:51:43 +0000

    feat: update database configuration to use PostgreSQL with dj_database_url
    - Replace SQLite database settings with PostgreSQL configuration using dj_database_url for improved scalability and performance.
    - Set connection parameters including maximum connection age for better resource management.

--- ll_project/settings.py
+++ ll_project/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -79,10 +80,10 @@ WSGI_APPLICATION = "ll_project.wsgi.application"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(
+        default="postgresql://postgres:postgres@localhost:5432/ll_project",
+        conn_max_age=600,
+    )
 }
 
 
